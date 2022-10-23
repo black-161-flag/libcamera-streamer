4681faf4a27c747f18eb454b389fe7a2d8110119
black-161-flag <github@blackbirds.xyz>
2022-10-23 20:32:17 +0200

    Add simplejpeg to install_requires

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     scripts=['bin/libcamera-streamer'],
     description='simple mpg streamer for libcamera',
     long_description=open('README.md').read(),
-    install_requires=["picamera2", 'numpy <= 1.23.4'],
+    install_requires=["picamera2", "simplejpeg", 'numpy <= 1.23.4'],
     package_dir={'libcamera-streamer': 'bin'},
 
 )
