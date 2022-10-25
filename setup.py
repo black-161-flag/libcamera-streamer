01fb9cf63a83e0815e370ef21b00594d99f5e731
black-161-flag <github@blackbirds.xyz>
2022-10-25 20:25:11 +0200

    Remove numpy version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='libcamera-streamer',
-    version='0.0.3',
+    version='0.0.4',
     packages=['libcamera-streamer'],
     scripts=['bin/libcamera-streamer'],
     description='simple mpg streamer for libcamera',
     long_description=open('README.md').read(),
-    install_requires=["picamera2", "simplejpeg", 'numpy <= 1.23.4'],
+    install_requires=["picamera2", "simplejpeg", 'numpy'],
     package_dir={'libcamera-streamer': 'bin'},
 
 )
